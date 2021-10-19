9229d4152f48501e458c41703e4d62cf18d17704
Marcel Michelfelder <marcel.michelfelder@team-parallax.com>
2021-10-19 09:15:49 +0200

    Init

---
+++ dist/GoogleFont.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require("prop-types");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var GoogleFont = function GoogleFont(props) {
+  // Create family + styles string
+  var fontsStr = "";
+  if (props.typography.options.googleFonts) {
+    var fonts = props.typography.options.googleFonts.map(function (font) {
+      var str = "";
+      str += font.name.split(" ").join("+");
+      str += ":";
+      str += font.styles.join(",");
+
+      return str;
+    });
+
+    fontsStr = fonts.join("|");
+
+    if (fontsStr) {
+      var fontDisplay = 'swap';
+      var validFontDisplayTypes = ['block', 'swap', 'fallback', 'optional'];
+      if (props.typography.options.fontDisplay && validFontDisplayTypes.includes(props.typography.options.fontDisplay)) {
+        fontDisplay = props.typography.options.fontDisplay;
+      }
+
+      fontsStr += "&display=" + fontDisplay;
+
+      return _react2.default.createElement("link", {
+        href: "//fonts.googleapis.com/css?family=" + fontsStr,
+        rel: "stylesheet",
+        type: "text/css"
+      });
+    }
+    return null;
+  }
+};
+
+GoogleFont.propTypes = {
+  typography: _propTypes2.default.object.isRequired
+};
+
+GoogleFont.displayName = "GoogleFont";
+
+module.exports = GoogleFont;
---
+++ src/GoogleFont.js
@@ -0,0 +1,46 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const GoogleFont = props => {
+  // Create family + styles string
+  let fontsStr = ""
+  if (props.typography.options.googleFonts) {
+    const fonts = props.typography.options.googleFonts.map(font => {
+      let str = ""
+      str += font.name.split(" ").join("+")
+      str += ":"
+      str += font.styles.join(",")
+
+      return str
+    })
+
+    fontsStr = fonts.join("|")
+
+    if (fontsStr) {
+      let fontDisplay = 'swap'
+      let validFontDisplayTypes = ['block', 'swap', 'fallback', 'optional']
+      if (props.typography.options.fontDisplay && validFontDisplayTypes.includes(props.typography.options.fontDisplay)) {
+        fontDisplay = props.typography.options.fontDisplay
+      }
+
+      fontsStr += `&display=${fontDisplay}`
+
+      return (
+        <link
+          href={`//fonts.googleapis.com/css?family=${fontsStr}`}
+          rel={"stylesheet"}
+          type={"text/css"}
+        />
+      )
+    }
+    return null
+  }
+}
+
+GoogleFont.propTypes = {
+  typography: PropTypes.object.isRequired,
+}
+
+GoogleFont.displayName = "GoogleFont"
+
+module.exports = GoogleFont
